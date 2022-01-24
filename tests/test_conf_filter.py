5f24b4bcb6f22dce72fe993b244047767cb3cca5
Han Wang <wang_han@iapcm.ac.cn>
2022-01-24 14:27:48 +0800

    add UT for conf filter

---
+++ tests/test_conf_filter.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import unittest, dpdata
+import context
+from dpgen2.utils.conf_filter import ConfFilter, ConfFilters
+
+class OKFilter(ConfFilter):
+    def check(
+            self,
+            conf : dpdata.System, 
+    )-> bool:
+        return True
+
+class FailedFilter(ConfFilter):
+    def check(
+            self,
+            conf : dpdata.System, 
+    )-> bool:
+        return False
+
+class TestConfFilter(unittest.TestCase):
+    def test_ok(self):
+        ok0 = OKFilter()
+        ok1 = OKFilter()
+        ok2 = OKFilter()
+        filters = ConfFilters()
+        filters.add(ok0).add(ok1).add(ok2)
+        self.assertTrue(filters.check(None))
+
+    def test_failed(self):
+        ok0 = OKFilter()
+        ok1 = FailedFilter()
+        ok2 = OKFilter()
+        filters = ConfFilters()
+        filters.add(ok0).add(ok1).add(ok2)
+        self.assertFalse(filters.check(None))
+
