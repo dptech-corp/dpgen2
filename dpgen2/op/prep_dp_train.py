311f5367c01f1d6a191608a76ca4327096920692
Han Wang <wang_han@iapcm.ac.cn>
2022-01-24 15:25:24 +0800

    add doc string for PrepDPTrain and RunDPTrain

--- dpgen2/op/prep_dp_train.py
+++ dpgen2/op/prep_dp_train.py
@@ -9,7 +9,19 @@ import json
 from typing import Tuple, List
 from pathlib import Path
 
-class PrepDPTrain(OP):    
+class PrepDPTrain(OP):
+    r"""Prepares the training scripts and working directories for training
+    DP models.
+
+    Inputs of the OP:
+    - `template_script`: A template of the training script.
+    - `numb_models`: Number of DP models to train.
+
+    Outputs of the OP:
+    - `task_names`: The name of tasks. The names of different tasks are different.
+    - `task_paths`: The paths to the tasks.
+    """
+
     @classmethod
     def get_input_sign(cls):
         return OPIOSign({
@@ -24,6 +36,10 @@ class PrepDPTrain(OP):
             "task_paths" : Artifact(List[Path]),
         })
 
-
-
-
+    @OP.exec_sign_check
+    def execute(
+            self,
+            ip : OPIO,
+    ) -> OPIO:
+        raise NotImplementedError
+    
--- dpgen2/op/run_dp_train.py
+++ dpgen2/op/run_dp_train.py
@@ -9,6 +9,26 @@ from typing import Tuple, List, Set
 from pathlib import Path
 
 class RunDPTrain(OP):
+    r"""Train and freeze one DP model. 
+
+    A working directory named `task_name` is created. All input files
+    are copied or symbol linked to `task_name`, and the DeePMD-kit
+    training and freezing tasks are exectuted from `task_name`.
+
+    Inputs of the OP:
+    - `task_name`: The name of training task.
+    - `task_path`: The path that contains all input files prepareed by `PrepDPTrain`.
+    - `init_model`: A frozen model to initialize the training.
+    - `init_data`: Initial training data.
+    - `iter_data`: Training data generated in the DPGEN iterations.
+
+    Outputs of the OP:
+    - `script`: The training script.
+    - `model`: The trained frozen model.
+    - `lcurve`: The learning curve file.
+    - `log`: The log file of training.
+    """
+
     @classmethod
     def get_input_sign(cls):
         return OPIOSign({
@@ -28,4 +48,10 @@ class RunDPTrain(OP):
             "log" : Artifact(Path),
         })
 
+    @OP.exec_sign_check
+    def execute(
+            self,
+            ip : OPIO,
+    ) -> OPIO:
+        raise NotImplementedError
 
