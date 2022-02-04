fda6ba66a20b2fd84924d94873f3b4969b8917b6
Han Wang <wang_han@iapcm.ac.cn>
2022-02-04 16:14:38 +0800

    implement prep_dp_train. add UTs.

--- dpgen2/op/prep_dp_train.py
+++ dpgen2/op/prep_dp_train.py
@@ -5,9 +5,13 @@ from dflow.python import (
     Artifact
 )
 
-import json
-from typing import Tuple, List
+import json, random, sys
+from typing import Tuple, List, Union
 from pathlib import Path
+from dpgen2.constants import (
+    train_task_pattern,
+    train_script_name,
+)
 
 class PrepDPTrain(OP):
     r"""Prepares the working directories for DP training tasks.
@@ -22,7 +26,7 @@ class PrepDPTrain(OP):
     @classmethod
     def get_input_sign(cls):
         return OPIOSign({
-            "template_script" : dict,
+            "template_script" : Union[dict, List[dict]],
             "numb_models" : int,
         })
 
@@ -45,7 +49,7 @@ class PrepDPTrain(OP):
         ip : dict
             Input dict with components:
 
-            - `template_script`: (`str`) A template of the training script.
+            - `template_script`: (`str` or `List[str]`) A template of the training script. Can be a `str` or `List[str]`. In the case of `str`, all training tasks share the same training input template, the only difference is the random number used to initialize the network parameters. In the case of `List[str]`, one training task uses one template from the list. The random numbers used to initialize the network parameters are differnt. The length of the list should be the same as `numb_models`.
             - `numb_models`: (`int`) Number of DP models to train.
         
         Returns
@@ -57,5 +61,45 @@ class PrepDPTrain(OP):
             - `task_paths`: (`Artifact(List[Path])`) The parepared working paths of the tasks. The order fo the Paths should be consistent with `op["task_names"]`
 
         """
-        raise NotImplementedError
-    
+        template = ip['template_script']
+        numb_models = ip['numb_models']
+        osubdirs = []
+        if type(template) != list:
+            template = [template for ii in range(numb_models)]
+        else:
+            if not (len(template) == numb_models):
+                raise RuntimeError(f'length of the template list should be equal to {numb_models}')
+
+        for ii in range(numb_models):
+            # mkdir
+            subdir = Path(train_task_pattern % ii) 
+            subdir.mkdir(exist_ok=True, parents=True)
+            osubdirs.append(str(subdir))
+            # change random seed in template
+            idict = self._script_rand_seed(template[ii])
+            # write input script
+            fname = subdir / train_script_name
+            with open(fname, 'w') as fp:
+                json.dump(idict, fp, indent = 4)
+
+        op = OPIO({
+            "task_names" : osubdirs,
+            "task_paths" : [Path(ii) for ii in osubdirs],
+        })
+        return op
+
+
+    def _script_rand_seed(
+            self,
+            input_dict,
+    ):
+        jtmp = input_dict.copy()
+        if jtmp['model']['descriptor']['type'] == 'hybrid':
+            for desc in jtmp['model']['descriptor']['list']:
+                desc['seed'] = random.randrange(sys.maxsize) % (2**32)
+        else:
+            jtmp['model']['descriptor']['seed'] = random.randrange(sys.maxsize) % (2**32)
+        jtmp['model']['fitting_net']['seed'] = random.randrange(sys.maxsize) % (2**32)
+        jtmp['training']['seed'] = random.randrange(sys.maxsize) % (2**32)
+        return jtmp
+
---
+++ tests/test_prep_dp_train.py
@@ -0,0 +1,170 @@
+import context
+import numpy as np
+import unittest, json
+from mock import mock
+
+from dflow.python import (
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact
+)
+from pathlib import Path
+from dpgen2.constants import (
+    train_task_pattern,
+    train_script_name,
+)
+from dpgen2.op.prep_dp_train import PrepDPTrain
+
+template_script_se_e2_a = \
+{
+    "model" : {
+        "descriptor": {
+            "type":     "se_e2_a",
+	    "seed":     1
+        },
+        "fitting_net" : {
+            "seed":     1
+	}
+    },
+    "training" : {
+        "systems":      [],
+        "set_prefix":   "set",
+        "stop_batch":   2000,
+        "batch_size":   'auto',
+	"seed":         1,
+    },
+}
+
+template_script_hybrid = \
+{
+    "model" : {
+        "descriptor": {
+            "type" : "hybrid",
+            "list": [
+                {
+                    "type":     "se_e2_a",
+	            "seed":     1
+                },
+                {
+                    "type":     "se_e3",
+	            "seed":     1
+                }
+            ]
+        },
+        "fitting_net" : {
+            "seed":     1
+	}
+    },
+    "training" : {
+        "systems":      [],
+        "set_prefix":   "set",
+        "stop_batch":   2000,
+        "batch_size":   'auto',
+	"seed":         1,
+    },
+}
+
+class faked_rg():
+    faked_random = -1
+    @classmethod
+    def randrange(cls,xx):
+        cls.faked_random += 1
+        return cls.faked_random
+
+class TestPrepDPTrain(unittest.TestCase):
+    def setUp(self):
+        self.ptrain = PrepDPTrain()
+
+    def _check_output_dir_and_file_exist(self, op, numb_models):
+        task_names = op['task_names']
+        task_paths = op['task_paths']
+        for ii in range(numb_models):
+            self.assertEqual(train_task_pattern % ii, task_names[ii])
+            self.assertEqual(Path(train_task_pattern % ii), task_paths[ii])
+            self.assertTrue(task_paths[ii].is_dir())
+            self.assertTrue((task_paths[ii] / train_script_name).is_file())
+
+    def test_template_str_hybrid(self):
+        numb_models = 2
+        ip = OPIO({
+            "template_script" : template_script_hybrid,
+            "numb_models" : numb_models
+        })
+
+        faked_rg.faked_random = -1
+        with mock.patch('random.randrange', faked_rg.randrange):
+            op = self.ptrain.execute(ip)
+        
+        self._check_output_dir_and_file_exist(op, numb_models)
+        
+        for ii in range(numb_models):
+            with open(Path(train_task_pattern % ii)/train_script_name) as fp:
+                jdata = json.load(fp)
+                self.assertEqual(jdata['model']['descriptor']['list'][0]['seed'], 4*ii+0)
+                self.assertEqual(jdata['model']['descriptor']['list'][1]['seed'], 4*ii+1)
+                self.assertEqual(jdata['model']['fitting_net']['seed'], 4*ii+2)
+                self.assertEqual(jdata['training']['seed'], 4*ii+3)
+        
+    def test_template_str_se_e2_a(self):
+        numb_models = 2
+        ip = OPIO({
+            "template_script" : template_script_se_e2_a,
+            "numb_models" : numb_models
+        })
+
+        faked_rg.faked_random = -1
+        with mock.patch('random.randrange', faked_rg.randrange):
+            op = self.ptrain.execute(ip)
+        
+        self._check_output_dir_and_file_exist(op, numb_models)
+        
+        for ii in range(numb_models):
+            with open(Path(train_task_pattern % ii)/train_script_name) as fp:
+                jdata = json.load(fp)
+                self.assertEqual(jdata['model']['descriptor']['seed'], 3*ii+0)
+                self.assertEqual(jdata['model']['fitting_net']['seed'], 3*ii+1)
+                self.assertEqual(jdata['training']['seed'], 3*ii+2)
+
+
+    def test_template_list_hyb_sea(self):
+        numb_models = 2
+        ip = OPIO({
+            "template_script" : [template_script_hybrid, template_script_se_e2_a],
+            "numb_models" : numb_models
+        })
+
+        faked_rg.faked_random = -1
+        with mock.patch('random.randrange', faked_rg.randrange):
+            op = self.ptrain.execute(ip)
+        
+        self._check_output_dir_and_file_exist(op, numb_models)
+        
+        ii = 0
+        with open(Path(train_task_pattern % ii)/train_script_name) as fp:
+            jdata = json.load(fp)
+            self.assertEqual(jdata['model']['descriptor']['list'][0]['seed'], 4*ii+0)
+            self.assertEqual(jdata['model']['descriptor']['list'][1]['seed'], 4*ii+1)
+            self.assertEqual(jdata['model']['fitting_net']['seed'], 4*ii+2)
+            self.assertEqual(jdata['training']['seed'], 4*ii+3)
+        ii = 1
+        with open(Path(train_task_pattern % ii)/train_script_name) as fp:
+            jdata = json.load(fp)
+            self.assertEqual(jdata['model']['descriptor']['seed'], 4*ii+0)
+            self.assertEqual(jdata['model']['fitting_net']['seed'], 4*ii+1)
+            self.assertEqual(jdata['training']['seed'], 4*ii+2)
+        
+
+    def test_template_raise_wrong_list_length(self):
+        numb_models = 2
+        ip = OPIO({
+            "template_script" : [template_script_hybrid, template_script_hybrid, template_script_se_e2_a],
+            "numb_models" : numb_models
+        })
+
+        with self.assertRaises(RuntimeError) as context:
+            faked_rg.faked_random = -1
+            with mock.patch('random.randrange', faked_rg.randrange):
+                op = self.ptrain.execute(ip)
+        self.assertTrue('length of the template list should be equal to 2' in str(context.exception))
+
