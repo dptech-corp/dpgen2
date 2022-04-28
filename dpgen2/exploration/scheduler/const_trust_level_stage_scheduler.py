bb232d73e44c5b5becc0cf12210eca62ab776600
Han Wang <92130845+wanghan-iapcm@users.noreply.github.com>
2022-04-28 21:44:03 +0800

    add option if raise fatal exception at max number of iteration (#13)
    Co-authored-by: Han Wang <wang_han@iapcm.ac.cn>

--- dpgen2/exploration/scheduler/const_trust_level_stage_scheduler.py
+++ dpgen2/exploration/scheduler/const_trust_level_stage_scheduler.py
@@ -18,6 +18,7 @@ class ConstTrustLevelStageScheduler(StageScheduler):
             trust_level : TrustLevel,
             conv_accuracy : float = 0.9,
             max_numb_iter : int = None,
+            fatal_at_max : bool = True,
     ):
         self.stage = stage
         self.trust_level = trust_level
@@ -25,6 +26,7 @@ class ConstTrustLevelStageScheduler(StageScheduler):
         self.selector = TrustLevelConfSelector(self.trust_level)
         self.nxt_iter = 0
         self.max_numb_iter = max_numb_iter
+        self.fatal_at_max = fatal_at_max
 
     def plan_next_iteration(
             self,
@@ -38,14 +40,19 @@ class ConstTrustLevelStageScheduler(StageScheduler):
             ret_selector = self.selector
         else :
             converged = report.accurate_ratio() >= self.conv_accuracy
+            if not converged:
+                # if not converged, check max iter
+                if self.max_numb_iter is not None and self.nxt_iter == self.max_numb_iter:
+                    if self.fatal_at_max:
+                        raise FatalError('reached maximal number of iterations')
+                    else:
+                        converged = True
+            # make lmp tasks
             if converged:
                 # if converged, no more lmp task
                 lmp_task_grp = None
                 ret_selector = None
-            else :
-                # if not converged, check max iter and make lmp tasks
-                if self.max_numb_iter is not None and self.nxt_iter == self.max_numb_iter:
-                    raise FatalError('reached maximal number of iterations')
+            else :                        
                 lmp_task_grp = self.stage.make_task()
                 ret_selector = self.selector
         self.nxt_iter += 1
